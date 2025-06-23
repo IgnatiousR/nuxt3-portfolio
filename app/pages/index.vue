cdeccd7a9f9fa251424907750d3015abb17cc21a
Ignatious <ignatiousr80@gmail.com>
2025-06-23 19:52:25 +0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,31 @@
 <template>
-    <div>Hello from Nuxt.</div>
+    <div>Main Page.</div>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: 'Main page',
+  description: 'Main Page',
+  ogTitle: 'Main Page',
+  ogDescription: '[og:description]',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: '[twitter:title]',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
