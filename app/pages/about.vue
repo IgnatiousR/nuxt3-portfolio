32de76d6307244ec8a5edbc071be6c886c19b934
Ignatious <ignatiousr80@gmail.com>
2025-06-23 19:52:18 +0600

    Create about.vue

---
+++ app/pages/about.vue
@@ -0,0 +1,23 @@
+<script setup>
+useHead({
+    title: 'About'
+})
+</script>
+
+<template>
+    <div>About me.</div>
+    <!-- <div>
+        <button @click="enableCustomLayout">Update layout</button>
+    </div> -->
+</template>
+
+<!-- <script setup>
+var v = true;
+// definePageMeta({
+//     layout: "another",
+// });
+
+// function enableCustomLayout () {
+//     setPageLayout('another')
+// }
+</script> -->
