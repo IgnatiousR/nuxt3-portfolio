d06c0fc4fb6585317c7ae9534e4c2f3c83874c7a
Ignatious <ignatiousr80@gmail.com>
2025-06-23 19:52:27 +0600

    Create projects.vue

---
+++ app/pages/projects.vue
@@ -0,0 +1,12 @@
+<script setup>
+useHead({
+    title: 'Projects'
+})
+</script>
+
+<template>
+    <div>
+        <h2 class="text-2xl font-semibold mb-10">Projects List</h2>
+        <ProjectList/>
+    </div>
+</template>
