f9604f9739999a1d40ead8fde4cd215f60b6e2aa
Ignatious <ignatiousr80@gmail.com>
2025-06-23 19:52:07 +0600

    Create menu.vue

---
+++ app/components/menu.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <ul class="flex space-x-4">
+            <li><NuxtLink to="/" class="p-1 hover:bg-neutral-200">Main</NuxtLink></li>
+            <li><NuxtLink to="/about" class="p-1 hover:bg-neutral-200">About</NuxtLink></li>
+            <li><NuxtLink to="/projects" class="p-1 hover:bg-neutral-200">Projects</NuxtLink></li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+/* .link {
+    p-1 hover:bg-neutral-200
+} */
+</style>
