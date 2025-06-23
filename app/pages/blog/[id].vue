c6300e9778e8d9c9ef8f2f57b3474c59569512a1
Ignatious <ignatiousr80@gmail.com>
2025-06-23 19:52:20 +0600

    Create [id].vue

---
+++ app/pages/blog/[id].vue
@@ -0,0 +1,9 @@
+<template>
+    <div>We are in a blog.</div>
+    <div>The post id is {{route.params.id}}.</div>
+</template>
+
+<script setup>
+const route = useRoute()
+// console.log(route)
+</script>
