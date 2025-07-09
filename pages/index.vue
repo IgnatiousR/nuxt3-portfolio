fa97700d0a710858867636233ec30ef5077a448a
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-09 15:20:15 +0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,39 @@
 <template>
-    <div class="font-bai">
-        <div>Index Page</div>
+    <div>
+        <ContentRenderer v-if="home" :value="home"/>
+        <div v-else>Index Page</div>
     </div>
 </template>
+
+<script setup>
+import { ContentRenderer } from '#components'
+
+const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
+console.log(home)
+
+useSeoMeta({
+  title: 'Main page',
+  description: 'Main Page',
+  ogTitle: 'Main Page',
+  ogDescription: '[og:description]',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: '[twitter:title]',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
