b6a34ae52131740febb749bfc3dadb7a3918079f
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-09 15:19:59 +0600

    Create nav-menu.vue

---
+++ components/nav-menu.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav>
+        <ul class="flex space-x-4">
+            <li><NuxtLink to="/" class="p-1 hover:bg-neutral-200 dark:hover:bg-neutral-600">Main</NuxtLink></li>
+            <li><NuxtLink to="/about" class="p-1 hover:bg-neutral-200 dark:hover:bg-neutral-600">About</NuxtLink></li>
+            <li><NuxtLink to="/projects" class="p-1 hover:bg-neutral-200 dark:hover:bg-neutral-600">Projects</NuxtLink></li>
+        </ul>
+    </nav>
+</template>
