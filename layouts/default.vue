5604864a44aa7598f83cab7df745afde87d7f8bf
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-10 15:21:49 +0600

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="container mx-auto my-2 px-2 font-bai">
         <header class="flex justify-between items-center">
-            <div class="flex items-center space-x-20">
                 <div><NuxtLink to="/" class="text-xl font-semibold p-1 hover:bg-neutral-200 dark:hover:bg-neutral-600">User</NuxtLink></div>
                 <NavMenu />
-            </div>
-            <ClientOnly><ColorModeSelector/> </ClientOnly>
+                <ClientOnly><ColorModeSelector/> </ClientOnly>
+            
         </header>
         <main class="p-2 mt-10">
             <slot />
