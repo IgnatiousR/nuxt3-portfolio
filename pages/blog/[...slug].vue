f505fbea5694e7df08cbd99b7bd72c8dd7961be3
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-10 15:21:59 +0600

    Update [...slug].vue

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,7 +1,14 @@
 <script setup>
-const slug = useRoute().params.slug
-const { data: post } = await useAsyncData(`blog-${slug}`, () => {
-  return queryCollection('blog').path(`/blog/${slug}`).first()
+//const slug = useRoute().params.slug
+const route = useRoute()
+// const firstPost = await queryCollection('blog').path('/blog/first').first()
+// console.log("FIrst: ",firstPost)
+// const { data: post } = await useAsyncData(`blog-${slug}`, () => {
+//   return queryCollection('blog').path(`/blog/${slug}`).first()
+// })
+
+const { data: post } = await useAsyncData(route.path, () => {
+  return queryCollection('blog').path(route.path).first()
 })
 
 </script>
